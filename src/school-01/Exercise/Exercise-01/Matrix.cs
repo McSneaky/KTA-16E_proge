c9e015b4b756dc69cd7f122b3ecb39eb97c4a48b
Margus Kevin Sünter <MargusKevin.Synter>
2017-10-23 14:28:46 +0300

    Start of basic Matrix class

---
+++ src/school-01/Exercise/Exercise-01/Matrix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_01
+{
+    class Matrix
+    {
+        public int x_len { get; set; }
+        public int y_len { get; set; }
+
+        public Matrix(int x, int y)
+        {
+            x_len = x;
+            y_len = y;
+        }
+
+        public static Matrix init()
+        {
+            Console.Out.WriteLine(
+                "Create new matrix "
+                + Environment.NewLine 
+                + "Insert msdatrix X and Y size"
+            );
+
+            // Get X
+            Console.Out.Write("X: ");
+            int x = int.Parse(Console.ReadLine());
+
+            // Get Y
+            Console.Out.Write("X: ");
+            int y = int.Parse(Console.ReadLine());
+
+            return new Matrix(x, y);
+        }
+    }
+}
---
+++ src/school-01/Exercise/Exercise-01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello world!");
+            
+            Matrix matrix = Matrix.init();
+
+            Console.Out.WriteLine("Generated matrix with "
+                + Environment.NewLine
+                + "X: " + matrix.x_len
+                + Environment.NewLine
+                + "Y: " + matrix.y_len
+            );
+
+            pause();
+        }
+
+        static void pause()
+        {
+            Console.Out.WriteLine("Press any key..");
+            Console.ReadKey();
+        }
+    }
+}
